47c640fbbaf4f6b3cc0a1cf7185805ceb5ca2e1e
Rajiv González <rajivgg91@gmail.com>
2018-01-02 09:50:35 -0600

    update

--- arduinoCode.ino
+++ arduinoCode.ino
@@ -1,3 +1,5 @@
+#include <LowPower.h>
+
 #include <MySIM800L.h>
 #include <QueueArray.h>
 #include <powerData.h>
@@ -7,31 +9,30 @@
 EnergyMonitor emon1;
 #define emonTxV3
 #define SAMPLES_HOUR 5 //1 hora=720 de estos si el timer es de 5s
-
 //SoftwareSerial mySerial2(11, 12); // RX, TX}
 SoftwareSerial serial(5, 6); //Rx, Tx
 MySIM800L gsm(11, 12);
 
-
 QueueArray<powerPackage> queue;//Datos no enviados
 //MySIM800L gsm(11, 12);
 
-int count3sec=0;
+int count10sec=0;
 float powerAcum=0;
 float kwH=0;
 //float not
 //struct 
 powerPackage currentPackage;
 const byte pinRel=  10;     
-
+bool timeOutFlag=false;
 //powerPackage currentPackage2;
 //int iTemp=0;
 void setup() {
   // put your setup code here, to run once:
+  analogReference(EXTERNAL);
   pinMode(pinRel, OUTPUT);
   digitalWrite(pinRel, LOW);//Se activa en bajo el relevador
   serial.begin(9600);
-  delay(1000);
+  delay(5000);
   serial.println("iniciando");
   /*for(;;)
   {
@@ -48,11 +49,25 @@ void setup() {
   //serial.println(gsm.http_method("0","http://9869d310.ngrok.io/celeste/device/status","application/json",{"DeviceId": "4001"}));
   serial.println(gsm.http_method("1","http://0d68a09a.ngrok.io","application/xml",data));//1 post
 */
-  emon1.current(CURRENT_PIN_1, 179.0);
-  emon1.voltage(VOLTAGE_PIN_1, 137.0, 1.7);  // Voltage: input pin, calibration, phase_shift
+
+  emon1.current(CURRENT_PIN_1, 112);//Con 179  funciona bien, probar con 90.9
+  emon1.voltage(VOLTAGE_PIN_1, 90.35, 1.7);  // Voltage: input pin, calibration, phase_shift
+  /*pinMode(A0, OUTPUT);
+  analogWrite(A0, 255);
+  
+  analogWrite(A0, 0);     // Pull input high low high then low to disburse any residual charge to avoid incorrectly detecting AC wave when not present
+  analogWrite(A0, 255);
+  analogWrite(A0, 0);     
+  analogWrite(A0, 255);
+  analogWrite(A0, 0);     
+  analogWrite(A0, 255);
+  analogWrite(A0, 0);     
+  pinMode(A0,INPUT);*/
+  delay(500);             //allow things to settle
+
   serial.print("sensores configurados: ");
   settleSensors();//Necesario por el filtro
-  Timer1.initialize(3000000);         // initialize timer1, and set a 5 seconds period
+  Timer1.initialize(5000000);         // initialize timer1, and set a 5 seconds period
   Timer1.attachInterrupt(callback);  // attaches callback() as a timer overflow interrupt
  // mySim.connectGSM();
 }
@@ -65,11 +80,11 @@ String createXML(String id, String potencia, String date)
 void loop() 
 {
   
-  if(count3sec==SAMPLES_HOUR)//1 hora=720 de estos si el timer es de 5s
+  if(count10sec==SAMPLES_HOUR)//1 hora=720 de estos si el timer es de 5s
   {
     Timer1.detachInterrupt();
     kwH=powerAcum/SAMPLES_HOUR;
-    count3sec=0;//Calcula kw hora
+    count10sec=0;//Calcula kw hora
     powerAcum=0;
     //serial.print("kw/h: ");
     //serial.println(kwH);
@@ -95,12 +110,12 @@ void loop()
     }
 
   }
-    /*serial.print("count3sec: ");
-    serial.println(count3sec);
+    /*serial.print("count10sec: ");
+    serial.println(count10sec);
     serial.print("modulo: ");
-    serial.println(count3sec%5);//Prueba cada 6*/
+    serial.println(count10sec%5);//Prueba cada 6*/
   
-    if(queue.isEmpty()==0 && count3sec%2==0 )//Cada minuto intenta reenviar la cola a menos que no haya datos
+    if(queue.isEmpty()==0 && count10sec%2==0 )//Cada minuto intenta reenviar la cola a menos que no haya datos
     {//Intenta enviar solo uno para que pueda atender las interrupciones del timer
       //Desencola
       // currentPackage.copyPackage(&(queue.dequeue()));
@@ -110,16 +125,20 @@ void loop()
       
     }
   
-  
-delay(1);
+ // LowPower.powerDown(SLEEP_2S, ADC_OFF, BOD_OFF);
+delay(10);
 }
 
 void callback()
 {
     // put your main code here, to run repeatedly:
-  emon1.calcVI(20, 2000);        // Calculate all. No.of wavelengths, time-out
+  emon1.calcVI(30, 2000, &timeOutFlag);        // Calculate all. No.of wavelengths, time-out, originalmente 20 y 2000
   //emon1.serialprint();           // Print out all variables
-     count3sec++;
+     count10sec++;
+     if(timeOutFlag)
+     {
+       serial.println("Timeout! ");
+     }
    serial.println("Potencia real= ");
    serial.println(emon1.realPower);
 
@@ -138,7 +157,7 @@ void settleSensors()
   for(int i=0; i<25; i++)
   {
     //serial.print("leyendo...");
-    emon1.calcVI(20, 2000);        
+    emon1.calcVI(25, 2000, &timeOutFlag);        
   }
   serial.print("filtro establecido: ");
 }
