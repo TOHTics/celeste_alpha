2b279681ec083e04a81c13518a160b8d9a339986
Rajiv91 <rajivgg91@gmail.com>
2018-01-15 13:43:37 -0600

    add power class

---
+++ celeste/alpha/Power.py
@@ -0,0 +1,14 @@
+#/usr/local/bin/python
+import serial
+from serial import SerialException
+import os
+import time
+import subprocess
+import requests
+import datetime
+import random
+import sys
+
+class Power:
+    def __init__(self):
+
